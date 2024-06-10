e5e48cc916816f3c781ffac9a3034562c144d52b
Guilherme Carvalho <carvalho_guilhermegomes@hotmail.com>
2024-06-10 18:36:34 -0300

    checking bugs in the projects

--- main.js
+++ main.js
@@ -1,19 +1,44 @@
-const randomNumber = Math.round(Math.random() * 10);
+//variaveis
+let randomNumber = Math.round(Math.random() * 10);
+const screen1 = document.querySelector(".screen1");
+const screen2 =  document.querySelector(".screen2");
+const btnTry = document.querySelector('#btnTry');
+const btnReset = document.querySelector('#newPlaying')
 console.log(randomNumber)
 let xAttempts = 1;
 
+//function
 function handleClick(event){
     event.preventDefault()
+    randomNumber;
+
     let getNumber = document.querySelector("#getNumber");
 
     if(Number(getNumber.value) === randomNumber){
-        document.querySelector(".screen1").classList.add("hide");
-        document.querySelector(".screen2").classList.remove('hide');
+        screen1.classList.add("hide");
+        screen2.classList.remove('hide');
 
         document
         .querySelector('.screen2 h2')
         .innerText = `acertou em ${xAttempts} tentativas`
     }
-
-    xAttempts++
+    getNumber.value = "";
+    xAttempts++;
 }
+
+function handleResetClick(){
+    toggleScreen()
+    xAttempts = 1;
+    randomNumber = Math.round(Math.random() * 10);
+console.log('reset', randomNumber)
+
+}
+
+function toggleScreen(){
+    screen1.classList.remove("hide");
+    screen2.classList.add('hide');
+}
+
+// eventos
+btnTry.addEventListener('click', handleClick);
+btnReset.addEventListener('click', handleResetClick);
